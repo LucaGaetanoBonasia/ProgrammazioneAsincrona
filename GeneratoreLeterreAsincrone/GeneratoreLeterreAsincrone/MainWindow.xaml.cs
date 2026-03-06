40e1817179dd75beb2f5d41ec71887b670d81926
Luca <lucabonasia.bonasia.stud@ispascalcomandini.it>
2026-03-06 09:50:50 +0100

    R.2: GRafica impostata

--- GeneratoreLeterreAsincrone/GeneratoreLeterreAsincrone/MainWindow.xaml.cs
+++ GeneratoreLeterreAsincrone/GeneratoreLeterreAsincrone/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,9 +17,26 @@ namespace GeneratoreLeterreAsincrone
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<char> lettere = new List<char>() {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        Random random = new Random();
+
+        int count = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
+                count= random.Next(0, 26);
+                this.Dispatcher.Invoke(() =>
+                {
+                    lblLettere.Content = lettere[count].ToString();
+                });
+            
+        }
+
+        private void btnEstrai_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
